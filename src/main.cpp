311adaa008a4e37d6858683e774b8998c894f1ba
いちごミルク2\2220287 <619345haruto@gmail.com>
2025-07-17 11:16:32 +0900

    cmake対応？

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <entt/entt.hpp>
+#include <iostream>
+
+int main()
+{
+    entt::registry registry;
+    entt::entity  e = registry.create();
+    std::cout << "Hello, EnTT! Entity ID = " << int(e) << '\n';
+    return 0;
+}
